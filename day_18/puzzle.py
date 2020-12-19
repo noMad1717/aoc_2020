3454d1ac95703c21083bde63a0892c2f7f714ff9
Erik Karlstrand <erik.jonsson17@gmail.com>
2020-12-19 14:20:56 +0100

    Solutions and tests for day 18

--- day_18/puzzle.py
+++ day_18/puzzle.py
@@ -4,6 +4,8 @@
 import re
 import util
 
+from collections import deque
+
 def solveLeftToRight(expr):
     ops = [x for x in expr if x == '+' or x == '*']
     nums = re.split(' [\+\*] ', expr)
@@ -11,23 +13,81 @@ def solveLeftToRight(expr):
     for i, op in enumerate(ops):
         rh = nums[i+1]
         lh = eval(f'{lh}{op}{rh}')
-    return int(lh)
+    return lh
 
-def solveParentheses(expr):
+def solveAdditionFirst(expr):
+    parts = expr.split(' * ')
+    for part in parts:
+        expr = expr.replace(part, str(eval(part)))
+    return eval(expr)
+
+def solveParentheses(expr, solveFunc):
     match = re.findall('\([\d \+\*]*\)', expr)
     if not match:
         return expr
     for m in match:
-        expr = expr.replace(m, str(solveLeftToRight(m[1:-1])))
-    return solveParentheses(expr)
+        expr = expr.replace(m, str(solveFunc(m[1:-1])))
+    return solveParentheses(expr, solveFunc)
+
+def peek(stack):
+    try:
+        return stack[-1]
+    except IndexError:
+        return None
+
+def shuntingYard(expr):
+    opStack = deque()
+    output = deque()
+    expr = expr.replace('(', '( ')
+    expr = expr.replace(')', ' )')
+    expr = expr.split()
+    for c in expr:
+        if re.match('\d', c):
+            output.append(c)
+        elif c == '+' or c == '*':
+            while len(opStack) > 0:
+                topOp = peek(opStack)
+                if topOp == '(' or (topOp == '*' and c == '+'):
+                    break
+                output.append(opStack.pop())
+            opStack.append(c)
+        elif c == '(':
+            opStack.append(c)
+        elif c == ')':
+            while len(opStack) > 0:
+                topOp = opStack.pop()
+                if topOp == '(':
+                    break
+                output.append(topOp)
+    while len(opStack) > 0:
+        output.append(opStack.pop())
+    return output
+
+def evalShuntingYard(output):
+    stack = deque()
+    for token in output:
+        if re.match('\d', token):
+            stack.append(int(token))
+        elif token == '+':
+            stack.append(stack.pop() + stack.pop())
+        else:
+            stack.append(stack.pop() * stack.pop())
+    return stack.pop()
 
 def partOne(data):
     res = 0
     for line in data:
-        res += solveLeftToRight(solveParentheses(line))
+        res += solveLeftToRight(solveParentheses(line, solveLeftToRight))
+    return res
+
+def partTwo(data):
+    res = 0
+    for line in data:
+        res += evalShuntingYard(shuntingYard(line))
     return res
 
 homework = util.fileToStringList('input')
 
 print(f'Part one: Sum of all expressions = {partOne(homework)}')
+print(f'Part two: Sum of all expressions = {partTwo(homework)}')
 
--- day_18/test.py
+++ day_18/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from puzzle import solveLeftToRight, solveParentheses, partOne
+from puzzle import solveLeftToRight, solveAdditionFirst, solveParentheses, partOne, partTwo
 
 
 class TestPuzzle(unittest.TestCase):
@@ -8,39 +8,68 @@ class TestPuzzle(unittest.TestCase):
     data = ['2 * 3 + (4 * 5)',
             '5 + (8 * 3 + 9 + 3 * 4 * 3)',
             '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
-            '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2']
+            '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
+            '1 + (2 * 3) + (4 * (5 + 6))']
 
     def testSolveLeftToRight(self):
         expectedFirst = 26
         expectedSecond = 437
         expectedThird = 12240
         expectedFourth = 13632
+        expectedFifth = 51
         resFirst = solveLeftToRight('2 * 3 + 20')
         resSecond = solveLeftToRight('5 + 432')
         resThird = solveLeftToRight('5 * 9 * 272')
         resFourth = solveLeftToRight('6810 + 2 + 4 * 2')
+        resFifth = solveLeftToRight('1 + 6 + 44')
         self.assertEqual(resFirst, expectedFirst)
         self.assertEqual(resSecond, expectedSecond)
         self.assertEqual(resThird, expectedThird)
         self.assertEqual(resFourth, expectedFourth)
+        self.assertEqual(resFifth, expectedFifth)
+
+    def testSolveAdditionFirst(self):
+        expectedFirst = 46
+        expectedSecond = 1445
+        expectedThird = 669060
+        expectedFourth = 23340
+        expectedFifth = 51
+        resFirst = solveAdditionFirst('2 * 3 + 20')
+        resSecond = solveAdditionFirst('5 + 1440')
+        resThird = solveAdditionFirst('5 * 9 * 14868')
+        resFourth = solveAdditionFirst('11664 + 2 + 4 * 2')
+        resFifth = solveAdditionFirst('1 + 6 + 44')
+        self.assertEqual(resFirst, expectedFirst)
+        self.assertEqual(resSecond, expectedSecond)
+        self.assertEqual(resThird, expectedThird)
+        self.assertEqual(resFourth, expectedFourth)
+        self.assertEqual(resFifth, expectedFifth)
 
     def testSolveParentheses(self):
         expectedFirst = '2 * 3 + 20'
         expectedSecond = '5 + 432'
         expectedThird = '5 * 9 * 272'
         expectedFourth = '6810 + 2 + 4 * 2'
-        resFirst = solveParentheses(self.data[0])
-        resSecond = solveParentheses(self.data[1])
-        resThird = solveParentheses(self.data[2])
-        resFourth = solveParentheses(self.data[3])
+        expectedFifth = '1 + 6 + 44'
+        resFirst = solveParentheses(self.data[0], solveLeftToRight)
+        resSecond = solveParentheses(self.data[1], solveLeftToRight)
+        resThird = solveParentheses(self.data[2], solveLeftToRight)
+        resFourth = solveParentheses(self.data[3], solveLeftToRight)
+        resFifth = solveParentheses(self.data[4], solveLeftToRight)
         self.assertEqual(resFirst, expectedFirst)
         self.assertEqual(resSecond, expectedSecond)
         self.assertEqual(resThird, expectedThird)
         self.assertEqual(resFourth, expectedFourth)
+        self.assertEqual(resFifth, expectedFifth)
 
     def testPartOne(self):
-        expected = 26335
+        expected = 26386
         result = partOne(self.data)
+        self.assertEqual(result, expected)
+
+    def testPartTwo(self):
+        expected = 693942
+        result = partTwo(self.data)
         self.assertEqual(result, expected)
 
 
