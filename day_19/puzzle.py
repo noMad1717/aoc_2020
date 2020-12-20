4da618a2556540766a8d9a19652c7a4fcb786660
Erik Karlstrand <erik.jonsson17@gmail.com>
2020-12-20 21:05:39 +0100

    Add hacky solution for day 19 part two

--- day_19/puzzle.py
+++ day_19/puzzle.py
@@ -1,10 +1,10 @@
 """
     Advent of Code 2020 - Day 19
 """
-import re
+import regex
 import util
 
-def getExpression(rule, rules, regexes, exprList, isTopRule):
+def getExpression(rule, ruleId, rules, exprList, isTopRule):
     if rule.startswith('"'):
         exprList.append(rule.replace('"', ''))
         return
@@ -17,7 +17,24 @@ def getExpression(rule, rules, regexes, exprList, isTopRule):
         if len(alternatives) > 1:
             exprList.append('(')
         for subRule in alt.split():
-            getExpression(rules[int(subRule)], rules, regexes, exprList, False)
+            if subRule == ruleId:
+                if subRule == '8':
+                    exprList.append('(?<eight>(')
+                    getExpression(rules[42], '42', rules, exprList, False)
+                    exprList.append('|')
+                    getExpression(rules[42], '42', rules, exprList, False)
+                    exprList.append('(?&eight)))')
+                elif subRule == '11':
+                    exprList.append('(?<eleven>(')
+                    getExpression(rules[42], '42', rules, exprList, False)
+                    getExpression(rules[31], '31', rules, exprList, False)
+                    exprList.append('|')
+                    getExpression(rules[42], '42', rules, exprList, False)
+                    exprList.append('(?&eleven)')
+                    getExpression(rules[31], '31', rules, exprList, False)
+                    exprList.append('))')
+            else:
+                getExpression(rules[int(subRule)], subRule, rules, exprList, False)
         if len(alternatives) > 1:
             if i < len(alternatives) - 1:
                 exprList.append(')|')
@@ -30,14 +47,13 @@ def getExpression(rule, rules, regexes, exprList, isTopRule):
 
 def parseRules(data):
     rules = {}
-    regexes = {}
     for line in data:
         if line == '':
             break
         parts = line.split(': ')
         rules[int(parts[0])] = parts[1]
     exprList = []
-    getExpression(rules[0], rules, regexes, exprList, True)
+    getExpression(rules[0], '0', rules, exprList, True)
     ruleExpression = ''.join(exprList)
     return ruleExpression
 
@@ -46,11 +62,23 @@ def partOne(data):
     startIndex = data.index('') + 1
     validMessages = 0
     for line in data[startIndex:]:
-        if re.match(pattern, line):
+        if regex.match(pattern, line):
+            validMessages += 1
+    return validMessages
+
+def partTwo(data):
+    data[data.index('8: 42')] = '8: 42 | 42 8'
+    data[data.index('11: 42 31')] = '11: 42 31 | 42 11 31'
+    pattern = parseRules(data)
+    startIndex = data.index('') + 1
+    validMessages = 0
+    for line in data[startIndex:]:
+        if regex.match(pattern, line):
             validMessages += 1
     return validMessages
 
 puzzleInput = util.fileToStringList('input')
 
-print(f'Part one: Found {partOne(puzzleInput)} valid messages!')
+#print(f'Part one: Found {partOne(puzzleInput)} valid messages!')
+print(f'Part two: Found {partTwo(puzzleInput)} valid messages!')
 
