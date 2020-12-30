78068f917e9124703ab95ad4751c508e4be4c29b
Erik Karlstrand <erik.jonsson17@gmail.com>
2020-12-30 14:19:45 +0100

    Updated solutions and tests for day 20

--- day_20/puzzle.py
+++ day_20/puzzle.py
@@ -9,61 +9,202 @@ from functools import reduce
 def parseInput(data):
     tiles = {}
     cur = ''
-    tileRows = []
-    data.append('')
     for line in data:
         if line == '':
-            if cur != '' and len(tileRows) > 0:
-                maxRow = len(tileRows) - 1
-                maxCol = len(tileRows[0]) - 1
-                borders = []
-                borders.append(tileRows[0])
-                borders.append(''.join([r[0] for r in tileRows]))
-                borders.append(tileRows[-1])
-                borders.append(''.join([r[-1] for r in tileRows]))
-                tiles[cur] = borders
-                cur = ''
-                tileRows = []
             continue
         if line.startswith('Tile '):
             cur = line[5:-1]
+            tiles[cur] = []
             continue
-        tileRows.append(line)
+        tiles[cur].append(line)
     return tiles
 
-def findMatches(tiles):
-    matches = {}
-    for key, borders in tiles.items():
-        m = set()
-        for k, b in tiles.items():
-            if key == k:
-                continue
-            try:
-                if key in matches[k]:
-                    m.add(k)
+def flipVertical(tiles, borders):
+    updatedBorders = {}
+    if borders:
+        updatedBorders = {'top': borders['btm'], 'rgt': borders['rgt'], 'btm': borders['top'], 'lft': borders['lft']}
+    return [row for row in reversed(tiles)], updatedBorders
+
+def flipHorizontal(tiles, borders):
+    updatedBorders = {}
+    if borders:
+        updatedBorders = {'top': borders['top'], 'rgt': borders['lft'], 'btm': borders['btm'], 'lft': borders['rgt']}
+    return [''.join(reversed(row)) for row in tiles], updatedBorders
+
+def rotateImg(tiles, borders):
+    rotated = []
+    updatedBorders = {}
+    maxCol = len(tiles[0])
+    for i in range(0, maxCol):
+        t = [row[i] for row in reversed(tiles)]
+        rotated.append(''.join(t))
+    updatedBorders = {}
+    if borders:
+        updatedBorders = {'top': borders['lft'], 'rgt': borders['top'], 'btm': borders['rgt'], 'lft': borders['btm']}
+    return rotated, updatedBorders
+
+def getBorders(tile):
+    borders = {}
+    borders['top'] = tile[0]
+    borders['lft'] = ''.join([r[0] for r in tile])
+    borders['btm'] = tile[-1]
+    borders['rgt'] = ''.join([r[-1] for r in tile])
+    return borders
+
+def findAdjacentTiles(tiles):
+    adjacents = {}
+    for tId, pixels in tiles.items():
+        tileBorders = {}
+        borders = getBorders(pixels)
+        for label, border in borders.items():
+            tileBorders[label] = None
+            for adjTileId, adjPixels in tiles.items():
+                if adjTileId == tId:
                     continue
-            except KeyError:
-                pass
-            for border in borders:
-                if border in b:
-                    m.add(k)
-                    continue
-                else:
-                    reverse = ''.join(reversed(border))
-                    if reverse in b:
-                        m.add(k)
-                        continue
-        matches[key] = m
-    return matches
+                adjBorders = getBorders(adjPixels)
+                borderSet = set(list(adjBorders.values()))
+                if border in borderSet or ''.join(reversed(border)) in borderSet:
+                    tileBorders[label] = adjTileId
+        adjacents[tId] = tileBorders
+    return adjacents
+
+def getTilesTopAndLeft(x, y, gridTiles):
+    topId = gridTiles[(x,y-1)] if y-1 >= 0 else None
+    leftId = gridTiles[(x-1,y)] if x-1 >= 0 else None
+    return topId, leftId
+
+def trimAndMerge(tiles, grid, y):
+    gridRowLen = len(tiles[1:-1])
+    gridLen = len(grid)
+    actualLen = (gridLen // gridRowLen) - 1 if gridLen > 0 else 0
+    for i, row in enumerate(tiles[1:-1]):
+        if y > actualLen or gridLen == 0:
+            grid.append(row[1:-1])
+        else:
+            actualRow = (y * gridRowLen) + i if actualLen > 0 else i
+            grid[actualRow] = ''.join([grid[actualRow], row[1:-1]])
+
+def addTileIdToGrid(tileId, x, y, gridTiles):
+    gridTiles[(x,y)] = tileId
+
+def updateBorders(borders):
+    return borders['top'], borders['lft']
+
+def addToGrid(tileId, x, y, tiles, adjacents, grid, gridTiles):
+    topId, leftId = getTilesTopAndLeft(x, y, gridTiles)
+    borders = adjacents[tileId]
+    adjTop = borders['top']
+    adjLeft = borders['lft']
+    imgTiles = tiles[tileId]
+    while not (adjTop == topId and adjLeft == leftId):
+        if topId != leftId:
+            if adjTop == topId:
+                imgTiles, borders = flipHorizontal(imgTiles, borders)
+                adjTop, adjLeft = updateBorders(borders)
+            elif adjLeft == leftId:
+                imgTiles, borders = flipVertical(imgTiles, borders)
+                adjTop, adjLeft = updateBorders(borders)
+            else:
+                imgTiles, borders = rotateImg(imgTiles, borders)
+                adjTop, adjLeft = updateBorders(borders)
+        else:
+            if adjTop == topId and adjLeft != adjTop:
+                imgTiles, borders = flipHorizontal(imgTiles, borders)
+                adjTop, adjLeft = updateBorders(borders)
+            elif adjLeft == leftId and adjLeft != adjTop:
+                imgTiles, borders = flipVertical(imgTiles, borders)
+                adjTop, adjLeft = updateBorders(borders)
+            else:
+                imgTiles, borders = rotateImg(imgTiles, borders)
+                adjTop, adjLeft = updateBorders(borders)
+    adjacents[tileId] = borders
+    trimAndMerge(imgTiles, grid, y)
+    addTileIdToGrid(tileId, x, y, gridTiles)
 
 def findCornerTiles(tiles):
-    corners = {int(x) for x in tiles.keys() if len(tiles[x]) == 2}
-    return corners
+    return [int(tId) for tId, borders in tiles.items() if len(list(filter((lambda x: x != None), borders.values()))) == 2]
+
+def buildImage(tiles):
+    adjacents = findAdjacentTiles(tiles)
+    cornerTiles = findCornerTiles(adjacents)
+    processed = set()
+    x = y = 0
+    grid = []
+    gridTiles = {}
+    prevTileId = currentTileId = None
+    while len(processed) < len(tiles):
+        if prevTileId == None:
+            currentTileId = str(cornerTiles[0])
+        else:
+            rightAdj = adjacents[prevTileId]['rgt']
+            if rightAdj != None:
+                currentTileId = rightAdj
+            else:
+                bottomAdj = adjacents[gridTiles[(0,y)]]['btm']
+                currentTileId = bottomAdj
+                x = 0
+                y += 1
+        addToGrid(currentTileId, x, y, tiles, adjacents, grid, gridTiles)
+        processed.add(currentTileId)
+        prevTileId = currentTileId
+        x += 1
+    return grid, gridTiles
+
+def scanGrid(grid, pattern):
+    patternLen = len(pattern)
+    patternRowLen = len(pattern[0])
+    matches = 0
+    y = 0
+    while y + (patternLen - 1) < len(grid):
+        found = False
+        offset = 0
+        while offset + patternRowLen < len(grid[y]):
+            found = True
+            for patternY, patternRow in enumerate(pattern):
+                if not found:
+                    break
+                x = 0
+                while x < patternRowLen:
+                    if patternRow[x] == '#' and grid[y+patternY][x+offset] != '#':
+                        found = False
+                        break
+                    x += 1
+            if not found:
+                offset += 1
+            else:
+                matches += 1
+                break
+        y += 1
+    return matches
+
+def findSeaMonster(grid):
+    pattern = ['                  # ',
+               '#    ##    ##    ###',
+               ' #  #  #  #  #  #   ']
+    totalHashes = 0
+    for row in grid:
+        totalHashes += row.count('#')
+    i = 0
+    matches = 0
+    while matches == 0 and i < 8:
+        matches = scanGrid(grid, pattern)
+        if matches == 0:
+            grid, b = rotateImg(grid, None)
+            if i == 3:
+                grid, b = flipVertical(grid, None)
+        i += 1
+    return totalHashes - (matches * 15)
 
 def partOne(data):
-    return reduce((lambda x,y: x*y), findCornerTiles(findMatches(parseInput(data))))
+    return reduce((lambda x,y: x*y), findCornerTiles(findAdjacentTiles(parseInput(data))))
+
+def partTwo(data):
+    tiles = parseInput(data)
+    grid, gridTiles = buildImage(tiles)
+    return findSeaMonster(grid)
 
 imageData = util.fileToStringList('input')
 
 print(f'Part one: Product of corner tile IDs = {partOne(imageData)}')
+print(f'Part two: Water roughness = {partTwo(imageData)}')
 
--- day_20/test.py
+++ day_20/test.py
@@ -1,31 +1,119 @@
 import util
 import unittest
 
-from puzzle import parseInput, findMatches, findCornerTiles, partOne
+from puzzle import findCornerTiles, rotateImg, parseInput, findAdjacentTiles, addToGrid, buildImage, partOne, partTwo
 
 
 class TestPuzzle(unittest.TestCase):
 
     data = util.fileToStringList('testinput')
+    data2 = ['#..',
+             '..#',
+             '.##']
+    borders = {'top': 1, 'rgt': 2, 'btm': 3, 'lft': 4}
 
-    def testParseInput(self):
-        expected = ['..##.#..#.', '.#####..#.', '..###..###', '...#.##..#']
-        result = parseInput(self.data)['2311']
+    def testFindAdjacentTiles(self):
+        expected = {'top': '1427', 'lft': '1951', 'btm': None, 'rgt': '3079'}
+        result = findAdjacentTiles(parseInput(self.data))['2311']
         self.assertEqual(result, expected)
 
-    def testFindMatches(self):
-        expected = set(['1951', '3079', '1427'])
-        result = findMatches(parseInput(self.data))['2311']
+    def testFindCornerTiles(self):
+        expected = [1951, 1171, 2971, 3079]
+        result = findCornerTiles(findAdjacentTiles(parseInput(self.data)))
         self.assertEqual(result, expected)
 
-    def testFindCornerTiels(self):
-        expected = set([1951, 3079, 2971, 1171])
-        result = findCornerTiles(findMatches(parseInput(self.data)))
-        self.assertEqual(result, expected)
+    def testRotateImg(self):
+        expectedTiles = ['..#',
+                         '#..',
+                         '##.']
+        expectedBorders = {'top': 4, 'rgt': 1, 'btm': 2, 'lft': 3}
+        resTiles, resBorders = rotateImg(self.data2, self.borders)
+        self.assertEqual(resTiles, expectedTiles)
+        self.assertEqual(resBorders, expectedBorders)
+
+    def testAddToGrid(self):
+        expectedGrid = ['.#.#..#.',
+                        '###....#',
+                        '##.##.##',
+                        '###.####',
+                        '##.#....',
+                        '...#####',
+                        '....#..#',
+                        '.####...']
+        expectedGridTiles = {(0,0): '1951'}
+        grid = []
+        gridTiles = {}
+        allTiles = parseInput(self.data)
+        adjacents = findAdjacentTiles(allTiles)
+        addToGrid('1951', 0, 0, allTiles, adjacents, grid, gridTiles)
+        self.assertEqual(grid, expectedGrid)
+        self.assertEqual(gridTiles, expectedGridTiles)
+
+    def testAddToGrid_withExisting(self):
+        expectedGrid = ['.#.#..#.##...#.#',
+                        '###....#.#....#.',
+                        '##.##.###.#.#..#',
+                        '###.#####...#.##',
+                        '##.#....#.##.###',
+                        '...########.#...',
+                        '....#..#...##..#',
+                        '.####...#..#....']
+        expectedGridTiles = {(0,0): '1951', (1,0): '2311'}
+        grid = ['.#.#..#.',
+                '###....#',
+                '##.##.##',
+                '###.####',
+                '##.#....',
+                '...#####',
+                '....#..#',
+                '.####...']
+        gridTiles = {(0,0): '1951'}
+        allTiles = parseInput(self.data)
+        adjacents = findAdjacentTiles(allTiles)
+        addToGrid('2311', 1, 0, allTiles, adjacents, grid, gridTiles)
+        self.assertEqual(grid, expectedGrid)
+        self.assertEqual(gridTiles, expectedGridTiles)
+
+    def testBuildImage(self):
+        expectedGrid = ['.#.#..#.##...#.##..#####',
+                        '###....#.#....#..#......',
+                        '##.##.###.#.#..######...',
+                        '###.#####...#.#####.#..#',
+                        '##.#....#.##.####...#.##',
+                        '...########.#....#####.#',
+                        '....#..#...##..#.#.###..',
+                        '.####...#..#.....#......',
+                        '#..#.##..#..###.#.##....',
+                        '#.####..#.####.#.#.###..',
+                        '###.#.#...#.######.#..##',
+                        '#.####....##..########.#',
+                        '##..##.#...#...#.#.#.#..',
+                        '...#..#..#.#.##..###.###',
+                        '.#.#....#.##.#...###.##.',
+                        '###.#...#..#.##.######..',
+                        '.#.#.###.##.##.#..#.##..',
+                        '.####.###.#...###.#..#.#',
+                        '..#.#..#..#.#.#.####.###',
+                        '#..####...#.#.#.###.###.',
+                        '#####..#####...###....##',
+                        '#.##..#..#...#..####...#',
+                        '.#.###..##..##..####.##.',
+                        '...###...##...#...#..###']
+        expectedGridTiles = {(0,0): '1951', (1,0): '2311', (2,0): '3079',
+                             (0,1): '2729', (1,1): '1427', (2,1): '2473',
+                             (0,2): '2971', (1,2): '1489', (2,2): '1171'}
+        resGrid, resGridTiles = buildImage(parseInput(self.data))
+        self.assertEqual(resGrid, expectedGrid)
+        self.assertEqual(resGridTiles, expectedGridTiles)
 
     def testPartOne(self):
         expected = 20899048083289
         result = partOne(self.data)
+        self.assertEqual(result, expected)
+
+    def testPartTwo(self):
+        expected = 273
+        result = partTwo(self.data)
         self.assertEqual(result, expected)
 
 
